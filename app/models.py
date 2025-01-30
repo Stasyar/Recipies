46dd6e623570ff70e0f1fcc7bfc547edfc764cec
Stasya <ap1050167@gmail.com>
2025-01-30 15:55:48 +0400

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer
+
+from database import Base
+
+
+class Recipy(Base):
+    __tablename__ = 'Recipy'
+    recipy_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    cooking_time = Column(Integer, index=True)
+    ingredients = Column(String, index=True)
+    description = Column(String, index=True)
+    views = Column(Integer, default=0)
+
