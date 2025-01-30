c0696a29efc7604ac2cd0e1daccf7d9ca7c082f5
Stasya <ap1050167@gmail.com>
2025-01-30 15:56:39 +0400

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+
+
+class BaseRecipy(BaseModel):
+    name: str = Field(..., description="Название рецепта")
+    cooking_time: int = Field(..., description="Время приготовления")
+
+
+class FirstWindow(BaseRecipy):
+    views: int = 0
+
+    class Config:
+        from_attributes = True
+
+
+class SecondWindow(BaseRecipy):
+    recipy_id: int = Field(..., description="id рецепта")
+    ingredients: str = Field(..., description="Ингридиенты рецепта")
+    description: str = Field(..., description="Описание рецепта")
+
+    class Config:
+        from_attributes = True
+
+
+class RecipyIn(BaseRecipy):
+    ingredients: str = Field(..., description="Ингридиенты рецепта")
+    description: str = Field(..., description="Описание рецепта")
+
+    class Config:
+        from_attributes = True
+
+
+
