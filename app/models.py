9577094c5b9641b6268fd2fd5c3d215db2faa56e
Stasya <ap1050167@gmail.com>
2025-01-30 16:52:54 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,11 +4,10 @@ from database import Base
 
 
 class Recipy(Base):
-    __tablename__ = 'Recipy'
+    __tablename__ = "Recipy"
     recipy_id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     cooking_time = Column(Integer, index=True)
     ingredients = Column(String, index=True)
     description = Column(String, index=True)
     views = Column(Integer, default=0)
-
