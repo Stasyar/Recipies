7b83f2adad2d7ca52d26eb1b92a1c41e6b2acbd3
Stasya <ap1050167@gmail.com>
2025-01-31 12:17:53 +0400

    ch9

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./routes.py.db"
+DATABASE_URL = "sqlite+aiosqlite:///./db.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
